4a7805298ad2ea02d1967afaa596ce5fdb2195bd
fernandogerezp-spec <fernandogerezp@gmail.com>
2026-04-02 18:49:34 -0400

    agregando formulario con grid

--- main.py
+++ main.py
@@ -1,42 +1,27 @@
 import tkinter as tk
-import os
-from dotenv import load_dotenv
-
-load_dotenv(".env")
-
-APP_USER = os.getenv("APP_USER")
-APP_PASSWORD = os.getenv("APP_PASSWORD")
 
 def capturar_credenciales():
-    user = entry_usuario.get()
-    password = entry_password.get()
-    validar_credenciales(user, password)
-
-def validar_credenciales(usuario, password):
-    if usuario == APP_USER and password == APP_PASSWORD:
-        mostrar_mensaje("Acceso correcto", "green")
-    else:
-        mostrar_mensaje("Usuario o contraseña incorrecta", "red")
-
-def mostrar_mensaje(texto, color):
-    label_resultado.config(text= texto, fg= color)
+    print("Botón presionado")
 
 ventana = tk.Tk()
-ventana.title("Practica 1: Validacion")
-ventana.geometry("400x400")
+ventana.title("Login")
+ventana.geometry("300x200")
 
-tk.Label(ventana, text= "Usuario:").pack()
+
+tk.Label(ventana, text="Usuario:").grid(row=0, column=0, padx=10, pady=5, sticky="w")
 entry_usuario = tk.Entry(ventana)
-entry_usuario.pack()
+entry_usuario.grid(row=0, column=1, padx=10, pady=5)
 
-tk.Label(ventana, text= "Contraseña:").pack()
-entry_password = tk.Entry(ventana)
-entry_password.pack()
 
-boton = tk.Button(ventana, text = "Validar Acceso", command=capturar_credenciales)
-boton.pack()
+tk.Label(ventana, text="Contraseña:").grid(row=1, column=0, padx=10, pady=5, sticky="w")
+entry_password = tk.Entry(ventana, show="*")
+entry_password.grid(row=1, column=1, padx=10, pady=5)
 
-label_resultado = tk.Label(ventana, text = "")
-label_resultado.pack()
 
+boton = tk.Button(ventana, text="Validar Acceso", command=capturar_credenciales)
+boton.grid(row=2, column=0, columnspan=2, pady=10)
+
+
+label_resultado = tk.Label(ventana, text="")
+label_resultado.grid(row=3, column=0, columnspan=2)
 ventana.mainloop()
